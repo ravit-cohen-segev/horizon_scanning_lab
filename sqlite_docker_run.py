e5e672941cf088c235e07b55ec63e6719558d826
ravit-cohen-segev <rnsc12@gmail.com>
2022-07-05 15:10:32 +0300

    updated file

--- sqlite_docker_run.py
+++ sqlite_docker_run.py
@@ -58,7 +58,8 @@ sql_file = df.to_sql(table_name, connection, if_exists='append', index=False)
 
 #%% create new df for updating the existing table in df
 
-df2 = pd.DataFrame(np.random.rand(3,5), columns=cols)
+df2 = pd.DataFrame(np.random.rand(3,6), columns=cols)
+df2['ID'] = np.array([1,3,7])
 
 #create temp table
 temp_table = 'temp_table'
@@ -69,14 +70,14 @@ engine.execute("CREATE TABLE IF NOT EXISTS {} {}".format(temp_table, table_cols)
 
 #insert new data into temp table. replace existing rows with new values
 
-sql1 = "UPDATE table_name AS t1, temp_table as t2 SET t1.col1 = t2.col1, \
-    t1.col2 = t2.col2, t1.col3 = t2.col3, t1.col4 = t2.col4. t1.col5 = t2.col5 \
-        WHERE t1.ID = t2.ID"
+sql1 = "UPDATE {} AS t1 JOIN {} as t2 SET t1.col1 = t2.col1, \
+    t1.col2 = t2.col2, t1.col3 = t2.col3, t1.col4 = t2.col4, t1.col5 = t2.col5 \
+        WHERE t2.ID = t1.ID".format(table_name, temp_table)
 
 engine.execute(sql1)
       
 #remove the same rows from temp_table 
-sql2 = "DELETE FROM t2 WHERE t2.ID = t1.ID"
+#sql2 = "DELETE FROM t2 WHERE t2.ID = t1.ID"
 
 #JOIN tables
 
