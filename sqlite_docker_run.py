67a1e281461f2c1ddf6116254745aeb15a7a0ce7
ravit-cohen-segev <rnsc12@gmail.com>
2022-07-05 14:05:09 +0300

    updated file

--- sqlite_docker_run.py
+++ sqlite_docker_run.py
@@ -6,6 +6,7 @@ This is a temporary script file.
 """
 
 import pandas as pd 
+import numpy as np
 import mysql.connector as msql
 from mysql.connector import Error
 import sqlalchemy as db
@@ -37,16 +38,47 @@ test_table = metadata.tables['test_table']
 
 #%% Read an example csv file I manually created and import csv into sql database
 
-path = r"C:\Users\97252\Documents\Ravit\example"
+path = r"C:\Users\97252\Documents\Ravit\horizon_scanning_lab\example"
     
 df = pd.read_csv(path)
 #drop Unnamed column
-df.drop('Unnamed: 0', axis=1, inplace=True)
+#df.drop('Unnamed: 0', axis=1, inplace=True)
 
 #name columns the same names as in db
-cols = ['col1', 'col2', 'col3','col4', 'col5']
+cols = ['ID', 'col1', 'col2', 'col3','col4', 'col5']
 df.columns = cols
 
-#convert to sql
-sql_file = df.to_sql('test_table',connection, if_exists='append', index=False) 
+#create table if doesn't exist
+table_name = 'test_table'
+table_cols ='(ID int NOT NULL PRIMARY KEY, col1 int, col2 int, col3 int, col4 int, col5 int)'
+engine.execute("CREATE TABLE IF NOT EXISTS {} {}".format(table_name, table_cols))
 
+#convert to sql and  import into the temporary table
+sql_file = df.to_sql(table_name, connection, if_exists='append', index=False)
+
+#%% create new df for updating the existing table in df
+
+df2 = pd.DataFrame(np.random.rand(3,5), columns=cols)
+
+#create temp table
+temp_table = 'temp_table'
+
+temp_table_cols ='(ID int NOT NULL PRIMARY KEY, col2 int)'
+
+engine.execute("CREATE TABLE IF NOT EXISTS {} {}".format(temp_table, table_cols))
+
+#insert new data into temp table. replace existing rows with new values
+
+sql1 = "UPDATE table_name AS t1, temp_table as t2 SET t1.col1 = t2.col1, \
+    t1.col2 = t2.col2, t1.col3 = t2.col3, t1.col4 = t2.col4. t1.col5 = t2.col5 \
+        WHERE t1.ID = t2.ID"
+
+engine.execute(sql1)
+      
+#remove the same rows from temp_table 
+sql2 = "DELETE FROM t2 WHERE t2.ID = t1.ID"
+
+#JOIN tables
+
+
+#engine.execute()
