f57cac83b028777cdba06adb8c78b50bc66aeb44
ravit-cohen-segev <rnsc12@gmail.com>
2022-07-06 13:07:27 +0300

    updated file

--- sqlite_docker_run.py
+++ sqlite_docker_run.py
@@ -50,11 +50,13 @@ df.columns = cols
 
 #create table if doesn't exist
 table_name = 'test_table'
-table_cols ='(ID int NOT NULL PRIMARY KEY, col1 int, col2 int, col3 int, col4 int, col5 int)'
+table_cols ='(ID int NOT NULL PRIMARY KEY, col1 float, col2 float, col3 float, col4 float, col5 float)'
+
+engine.execute("DROP TABLE IF EXISTS {}".format(table_name))
 engine.execute("CREATE TABLE IF NOT EXISTS {} {}".format(table_name, table_cols))
 
 #convert to sql and  import into the temporary table
-sql_file = df.to_sql(table_name, connection, if_exists='append', index=False)
+df.to_sql(table_name, connection, if_exists='append', index=False)
 
 #%% create new df for updating the existing table in df
 
@@ -67,19 +69,25 @@ temp_table = 'temp_table'
 temp_table_cols ='(ID int NOT NULL PRIMARY KEY, col2 int)'
 
 engine.execute("CREATE TABLE IF NOT EXISTS {} {}".format(temp_table, table_cols))
+df2.to_sql(temp_table, connection, if_exists='replace', index=False)
 
-#insert new data into temp table. replace existing rows with new values
+
+#%%insert new data into temp table. replace existing rows with new values
 
 sql1 = "UPDATE {} AS t1 JOIN {} as t2 SET t1.col1 = t2.col1, \
     t1.col2 = t2.col2, t1.col3 = t2.col3, t1.col4 = t2.col4, t1.col5 = t2.col5 \
         WHERE t2.ID = t1.ID".format(table_name, temp_table)
 
 engine.execute(sql1)
-      
-#remove the same rows from temp_table 
-#sql2 = "DELETE FROM t2 WHERE t2.ID = t1.ID"
+   
+#%%remove the same rows from temp_table 
+sql2 = "DELETE FROM temp_table WHERE temp_table.ID = test_table.ID"
 
-#JOIN tables
+
+engine.execute(sql2)
+   
+
+
 
 
 #engine.execute()
