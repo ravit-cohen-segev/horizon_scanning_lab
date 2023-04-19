3a381acedc01ac469abbb12d71a4bb14e4489470
RCS <67485297+ravit-cohen-segev@users.noreply.github.com>
2023-04-19 16:18:24 +0300

    Add files via upload

--- timeline_tweets_from_domains/__init__.py
+++ timeline_tweets_from_domains/__init__.py
@@ -14,27 +14,46 @@ from myfunctions import *
 
 
 # In[]
-NYT_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\Scrapers\NYT_RCS\docs\headlines\csv_converted_files'
-TN_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\technology_networks_scrapped'
+# read synonyms
+tech_syns = pd.read_csv(r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\9March23_tech_synonyms_and_names - Sheet1.csv').set_index('Tech_Name')
+
+# In[]
+#create TN timeline syn counts
 
 tech_syns = pd.read_csv(r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\9March23_tech_synonyms_and_names - Sheet1.csv').set_index('Tech_Name')
-# In[]
+TN_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\timeline_datasets\technology_networks_scrapped'
 
-
-
-
-
-
-
+tn_counts = time_series_tech_tweets(tech_syns).count_techs_in_text_tn(TN_path)  
 
 # In[]
+tn_counts.to_csv(r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\19Apr23_technology_networks_tech_counts_timeseries.csv')
 
+
+# In[]
+NYT_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\Scrapers\NYT_RCS\docs\headlines\csv_converted_files'
+
+# In[]
+
+df_counts_NYT = create_tech_list_timeseries_news_titles_NYT(tech_syns, NYT_path)
+
+
+
+
+
+
+# In[]
+'''
 tech_dict = tech_names.T.to_dict()
 
 for key in tech_dict.keys():
     tech_values = tech_dict[key].values()
     tech_values = [val for val in tech_values if type(val) is str]
-    tech_dict[key] = tech_values
+    tech_dict[key] = tech_values'''
     
 # In[]
-df_counts_NYT = create_tech_list_timeseries_news_titles_NYT(tech_dict, NYT_path)
+
+
+# In[]
+
+
+# In[]
--- timeline_tweets_from_domains/myfunctions.py
+++ timeline_tweets_from_domains/myfunctions.py
@@ -7,6 +7,7 @@ Created on Tue Feb 28 12:51:54 2023
 
 import pandas as pd
 import numpy as  np
+import re
 import ssl
 import json
 import os
@@ -147,11 +148,12 @@ def create_tech_list_timeseries_news_titles_techcrunch(dict_tech_names, domain_d
                         
                     
 # In[]
-
-def create_tech_list_timeseries_news_titles_NYT(dict_tech_names, domain_dfs_path):
+#TODO COMPLETE
+def create_tech_list_timeseries_news_titles_NYT(tech_syns, domain_dfs_path):
+    # concat all separate NYT csv files into one
     file_names = os.listdir(domain_dfs_path)
     
-    df_counts = create_empty_timeseries_df_with_tech_names(dict_tech_names.keys())
+    df_counts = create_empty_timeseries_df_with_tech_names(tech_syns.index)
     
     dfs = pd.DataFrame([]) 
     
@@ -161,7 +163,8 @@ def create_tech_list_timeseries_news_titles_NYT(dict_tech_names, domain_dfs_path
         df['pub_date'] = pd.to_datetime(df['pub_date'])
         
         dfs = pd.concat([dfs, df])
-               
+        
+    #convert dates                
     dates_list = dfs['pub_date'].to_list()
     dates_list = [calendar.month_abbr[date.month] + '-' + str(date.year)[-2:] for date in dates_list]
     dfs['pub_date'] = dates_list
@@ -169,89 +172,27 @@ def create_tech_list_timeseries_news_titles_NYT(dict_tech_names, domain_dfs_path
     #remove rows with no text
     dfs = dfs[dfs['abstract'].notna()]
     
-    
-    for key in dict_tech_names.keys():
-        syns_list = [key] + dict_tech_names[key]
-        
-        for i, row in dfs.iterrows():     
-            date_col = row['pub_date']
-            text = row['abstract']
-            for syn in syns_list:
-                if syn.lower() in text:
-                    try:
-                        df_counts.loc[key][date_col] += 1
-                        break
-                    except:
-                        print(date_col + ' not in requested dates')
-    return df_counts
- 
-# In[]
-
-#def search_syns_in_titles_abstracts(df, tech_syns)
-
-def create_tech_list_timeseries_tech_networks(tech_syns, path):   
-    df_counts = create_empty_timeseries_df_with_tech_names(tech_syns.index)
-    tn_files = os.listdir(path)
-    dfs = pd.concat([pd.read_csv(os.path.join(path,file)) for file in tn_files])
-   
-    
-   # remove 'Published: ' string from date column
-    dates_list = dfs['date'].to_list()
-    
-    dates_list = [date.replace('Published: ', '').replace('Last Updated: ', '').replace('  ','').replace('/n', '').replace('|', '') for date in dates_list]
-
-    dfs['date'] = dates_list
-    
-    for i, date in enumerate(dates_list):
-        try:
-            dfs['date'].iloc[i] = pd.to_datetime(date).strftime("%b-%y")
-        except:
-            date_split = list(set(dates_list[i].split()))
-            month = ''
-            year = ''
-            for d in date_split:
-                if d.isalpha():
-                    month = d
-                elif len(d)==4:
-                    year = d
-            dfs['date'].iloc[i] = pd.to_datetime('November' + ' ' + '2021').strftime("%b-%y")
-
-    df_counts = create_empty_timeseries_df_with_tech_names(tech_syns.index)
-    
-    search_dates = [col for col in df_counts.columns if col in dfs['date'].to_list()]
-    
-    df_counts.drop([col for col in df_counts if col not in search_dates], axis=1, inplace=True)
- 
-
-### TODO FIX NEXT LINES ####    
     all_mentioned_techs = []
     
-    for i, row in tqdm(dfs.iterrows()):
-        title = row['title']
-        summary = row['summary']
+    for i, row in dfs.iterrows():     
+        date_col = row['pub_date']
+        text = row['abstract']
         mentioned_techs = []
         
         for tech, tech_row  in tech_syns.iterrows():
-
             row_syns = list(set([tech] +  tech_row.dropna().to_list()))
+            
             for syn in row_syns:
-                
                 if syn == syn.upper():
                     #syn is acronym
                     syn = ' ' + syn + ' '
-                    if (syn in title) or (syn in summary):
+                    if syn in text:
                         mentioned_techs += [tech]
-                       
-                    
+                                    
                 else:
                     syn = syn.lower()
-                    if ((syn in title.lower()) or (syn in summary.lower())):
+                    if syn in text.lower():
                         mentioned_techs += [tech]
-                   
-                   
-          
-                
-        all_mentioned_techs.append(mentioned_techs)
     
     dfs['mentioned_techs'] = all_mentioned_techs 
     
@@ -262,18 +203,166 @@ def create_tech_list_timeseries_tech_networks(tech_syns, path):
         
         for key in count_techs.keys():
             df_counts.loc[key][date] = count_techs[key]
-        
+    
+    
+     
     return df_counts
-                    
 
 
 # In[]
 
-TN_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\technology_networks_scrapped'
+class time_series_tech_tweets():
+    def __init__(self, tech_syns, start_date = "2015-01-01", end_date = "2023-02-28"):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.tech_syns = tech_syns
+    
+
+    def create_empty_df_counts(self):
+        '''{input: tech list, articles from a certain domain 
+        output: empty df for counting number of mention in news}'''
+    
+
+        dates = [self.start_date, self.end_date]
+        tech_names = self.tech_syns.index
+    
+        start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
+        month_list = [i.strftime("%b-%y") for i in pd.date_range(start=self.start_date, end=self.end_date, freq='MS')]
+
+
+        df_counts = pd.DataFrame(np.zeros((len(tech_names), len(month_list))), columns = month_list)
+        df_counts.index = tech_names
+        return df_counts
+     
+    
+    def create_date_list(self,  dfs, date_col):
+        dates_list = dfs[date_col].to_list()
+        dates_list = [date.replace('Published: ', '').replace('Last Updated: ', '').replace('  ','').replace('/n', '').replace('|', '') for date in dates_list]
+        return dates_list
+
+    def count_techs_in_text_tn(self, tn_path, date_col='date'):
+        #read all csv files and concat into 1
+        tn_files = os.listdir(tn_path)
+        dfs = pd.concat([pd.read_csv(os.path.join(tn_path,file)) for file in tn_files])
+        dates_list = self.create_date_list(dfs, date_col)
+        
+        for i, date in enumerate(dates_list):
+            try:
+                dfs[date_col].iloc[i] = pd.to_datetime(date).strftime("%b-%y")
+            except:
+                date_split = list(set(dates_list[i].split()))
+                month = ''
+                year = ''
+                for d in date_split:
+                    if d.isalpha():
+                        month = d
+                    elif len(d)==4:
+                        year = d
+                dfs['date'].iloc[i] = pd.to_datetime('November' + ' ' + '2021').strftime("%b-%y")
+
+        df_counts = self.create_empty_df_counts()
+        
+        #drop dates that don't exist in dataset
+        search_dates = [col for col in df_counts.columns if col in dfs[date_col].to_list()]
+        
+        df_counts.drop([col for col in df_counts if col not in search_dates], axis=1, inplace=True)
+        
+        
+        # what techs are in every article
+        all_mentioned_techs = []
+        
+        for i, row in tqdm(dfs.iterrows()):
+            title = row['title']
+            summary = row['summary']
+            mentioned_techs = []
+            
+            for tech, tech_row  in self.tech_syns.iterrows():
+
+                row_syns = list(set([tech] +  tech_row.dropna().to_list()))
+                for syn in row_syns:
+                    
+                    if syn == syn.upper():
+                        #syn is acronym
+                        syn = ' ' + syn + ' '
+                        if (syn in title) or (syn in summary):
+                            mentioned_techs += [tech]
+                           
+                        
+                    else:
+                        syn = syn.lower()
+                        if ((syn in title.lower()) or (syn in summary.lower())):
+                            mentioned_techs += [tech]
+         
+            all_mentioned_techs.append(mentioned_techs)      
+        dfs['mentioned_techs'] = all_mentioned_techs 
+        
+        for date in search_dates:
+            temp_df = dfs[dfs['date']==date]
+            date_techs = temp_df['mentioned_techs'].to_list()
+            count_techs = Counter(list(itertools.chain.from_iterable(date_techs)))
+            
+            for key in count_techs.keys():
+                df_counts.loc[key][date] = count_techs[key]
+        
+        return df_counts
+    
+    
+    def is_syn_in_text(self, syn_list, text):
+        
+        for syn in syn_list:
+            if syn == syn.upper():
+                #syn is acronym
+                syn = ' ' + syn + ' '
+                if syn in text:
+                    return True
+                
+            else:
+                syn = syn.lower()
+                if syn in text.lower():
+                    return True
+        return False
+        
+    
+    
+    def count_techs_in_text_NYT(self, NYT_path, date_col=''):
+        # read each csv. date and month is in file name
+        file_names = os.listdir(NYT_path)
+        
+        df_counts = self.create_empty_df_counts()
+        
+        all_mentioned_techs = []
+        
+        for file in file_names:
+            file_path = os.path.join(NYT_path, file)
+            df = pd.read_csv(file_path)
+            date_str = re.sub("[^0-9]", "", file)
+            year, month = date_str[:4], date_str[4:]
+            date = datetime.strptime(year +'-' +month, '%Y-%m')
+            date = date.strftime("%b-%y")
+            mentioned_techs = []
+            
+            # combine all texts from a month into one string
+            # fix bad parsing while doing it
+            abstracts = df['abstract'].to_list()
+            
+            
+            abstracts = [remove_non_ascii(abstract) for abstract in abstracts if abstract is not None]
+            
+            for tech, tech_row in self.tech_syns.iterrows():
+                syn_list = list(set([tech] +  tech_row.dropna().to_list()))
+                
+                for text in abstracts:
+                    if self.is_syn_in_text:
+                        df_counts.loc[tech][date] += 1
+                        
+        return df_counts
+                        
+# In[]
 
 tech_syns = pd.read_csv(r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\9March23_tech_synonyms_and_names - Sheet1.csv').set_index('Tech_Name')
-# In[]
+NYT_path = r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\Scrapers\NYT_RCS\docs\headlines\csv_converted_files'
 
-tn_counts = create_tech_list_timeseries_tech_networks(tech_syns, TN_path)
 # In[]
-tn_counts.to_csv(r'C:\Users\Ravit\Documents\rnd\horizon_scanning_lab\feature_scripts\docs\15March23_technology_networks_tech_counts_timeseries.csv')
+NYT_counts = time_series_tech_tweets(tech_syns).count_techs_in_text_NYT(NYT_path)   
+        
+     
